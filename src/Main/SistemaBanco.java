79790bf2fabeeba1f5162a6bf54346b86358227f
apilar <abelgranda@hotmail.es>
2020-06-10 21:52:09 -0500

    Add files via upload

---
+++ src/Main/SistemaBanco.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ * @author aagranda
+ */
+public class SistemaBanco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Formularios.Menu menu = new Formularios.Menu();
+        menu.setVisible(true);
+        //gui.Login login = new gui.Login();
+        //login.setVisible(true);
+    }
+}
